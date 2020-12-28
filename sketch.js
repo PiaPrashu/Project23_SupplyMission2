bfc9befbb3276e9e16860983d98607d7fd6f5ed2
PiaPrashu <PiaPrashu@users.noreply.github.com>
2020-12-28 16:33:11 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,92 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground, leftSide, rightSide, bottomSide;
+var leftSideSprite, rightSideSprite, bottomSideSprite;
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+	
+
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.2, isStatic:true});
+	World.add(world, packageBody);
+
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+	leftSideSprite = createSprite(width/2-75,height/2+260,20,100);
+	leftSideSprite.shapeColor = "red";
+
+	leftSide = Bodies.rectangle(width/2-110,height/2+260,20,100,{isStatic:true});
+	World.add(world, leftSide);
+
+	bottomSideSprite = createSprite(width/2+30,height/2+300,200,20);
+	bottomSideSprite.shapeColor = "red";
+
+	bottomSide = Bodies.rectangle(width/2,height/2+300,200,20,{isStatic:true});
+	World.add(world, bottomSide);
+
+	rightSideSprite = createSprite(width/2+135,height/2+260,20,100);
+	rightSideSprite.shapeColor = "red";
+
+	rightSide = Bodies.rectangle(width/2+100,height/2+260,20,100,{isStatic:true});
+	World.add(world, rightSide);
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  
+  background(0);
+  packageSprite.x= packageBody.position.x;
+  packageSprite.y= packageBody.position.y;
+
+  leftSideSprite.x = leftSide.position.x;
+  leftSideSprite.y = leftSide.position.y;
+
+  rightSideSprite.x = rightSide.position.x;
+  rightSideSprite.y = rightSide.position.y;
+
+  bottomSideSprite.x = bottomSide.position.x;
+  bottomSideSprite.y = bottomSide.position.y;
+
+  drawSprites();
+ 
+}
+
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+	Matter.Body.setStatic(packageBody,false);
+  }
+}
+
+
+
